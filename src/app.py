93d38586f6f9ca9e2737c85c324657b4fce96e90
Jesse Cheng <jc2755@cornell.edu>
2024-12-11 13:40:20 -0500

    Add routes for posting and getting health data

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 from db import db
-from flask import Flask
+from flask import Flask, request
+import json
+from db import HealthData
+from datetime import datetime
 
 app = Flask(__name__)
 db_filename = "cms.db"
@@ -12,9 +15,50 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+# Generalized response formats
+def success_response(data, code=200):
+    return json.dumps({"success": True, "data": data}), code
 
-# your routes here
+def failure_response(message, code=404):
+    return json.dumps({"success": False, "error": message}), code
 
+# -- ROUTES ------------------------------------------------------------
+
+@app.route("/")
+def base():
+  return "hello world"
+
+@app.route("/data/", methods=["POST"])
+def create_data():
+  body = json.loads(request.data)
+  heart_rate = body.get("heart_rate")
+  blood_oxygen = body.get("blood_oxygen")
+  glucose_level = body.get("glucose_level")
+  hrv = body.get("hrv")
+  longitude = body.get("longitude")
+  latitude = body.get("latitude")
+
+  new_data = HealthData(
+        heart_rate=heart_rate,
+        blood_oxygen=blood_oxygen,
+        glucose_level=glucose_level,
+        hrv=hrv,
+        latitude=latitude,
+        longitude=longitude,
+        timestamp = datetime.now()
+    )
+  db.session.add(new_data)
+  db.session.commit()
+  return success_response(new_data.serialize(), 201)
+
+@app.route("/data/<int:user_id>/")
+def get_data(user_id):
+  # Check if data exists 
+  data = HealthData.query.filter_by(id=user_id).first()
+  if data is None:
+    return failure_response("Data not found")
+  
+  return success_response(data.serialize())
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- src/db.py
+++ src/db.py
@@ -3,3 +3,44 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # your classes here
+
+class HealthData(db.Model):
+  """
+  HealthData Model 
+  """
+  __tablename__ = "health_data"
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  heart_rate = db.Column(db.Integer, nullable=False)  # Heart rate in beats per minute
+  blood_oxygen = db.Column(db.Float, nullable=False)  # Blood oxygen level as a percentage
+  hrv = db.Column(db.Float, nullable=True)  # Heart rate variability in milliseconds
+  glucose_level = db.Column(db.Float, nullable = True) # Glucose level in mg/dL 
+  latitude = db.Column(db.Float, nullable=False)  # Latitude of the location
+  longitude = db.Column(db.Float, nullable=False)  # Longitude of the location
+  timestamp = db.Column(db.DateTime, nullable=False)  # Timestamp of data collection
+
+  def __init__(self, **kwargs):
+    """
+    Initializes a healthdata object
+    """
+    self.heart_rate = kwargs.get("heart_rate", 0)
+    self.blood_oxygen = kwargs.get("blood_oxygen", 0)
+    self.hrv = kwargs.get("hrv", 0)
+    self.glucose_level = kwargs.get("glucose_level", 0)
+    self.longitude = kwargs.get("longitude", 0)
+    self.latitude = kwargs.get("latitude", 0)
+    self.timestamp = kwargs.get("timestamp", "")
+
+  def serialize(self):
+    """ 
+    Serialize a healthdata object 
+    """
+    return {
+      "id": self.id,
+      "heart_rate": self.heart_rate,
+      "blood_oxygen": self.blood_oxygen,
+      "hrv": self.hrv,
+      "glucose_level": self.glucose_level,
+      "latitude": self.latitude, 
+      "longitude": self.longitude,
+      "timestamp": self.timestamp.isoformat()
+    }
